0f607f308b4b9bcc27e2aa8e71a65f46146fa232
Sergio D. Barrera <sdbeng@gmail.com>
2021-05-28 10:53:57 -0700

    update image folder path

---
+++ components/home-page/hero.js
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import classes from './hero.module.css'
+
+function Hero() {
+    return (
+        <section className={classes.hero}>
+        <div className={classes.image}>
+            <Image src="/images/site/serg_profile-ateam.png" alt="serg image" width={600} height={600}/>
+        </div>
+        <h1>Hi, I'm Serg</h1>
+        <p>
+            I like to write about full stack development and other news. Also, I love to keep everyone updated with the latest tips or tech events that may interest developers.
+        </p>
+            
+        </section>
+    )
+}
+
+export default Hero
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Hero from '../components/home-page/hero'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -15,6 +16,7 @@ export default function Home() {
         <h1 className={styles.title}>
           Summer 2021 Developer Bootcamp
         </h1>
+        <Hero />
 
         
 
