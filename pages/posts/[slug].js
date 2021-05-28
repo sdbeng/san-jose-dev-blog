a5a5ba5591e8fe69525d6f5d7926396847f8cf49
Sergio D. Barrera <sdbeng@gmail.com>
2021-05-28 08:31:44 -0700

    add the slug: single post page

---
+++ pages/posts/[slug].js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SinglePostPage() {
+    return (
+        <div>
+            <h2>SinglePostPage</h2>
+        </div>
+    )
+}
+
+export default SinglePostPage
