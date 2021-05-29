578eb32c528442f7635c0a9631a542a8fac89a53
Sergio D. Barrera <sdbeng@gmail.com>
2021-05-29 06:42:49 -0700

    fix bug

---
+++ components/layout/logo.js
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+function Logo() {
+    return (
+        <div>
+            <h2>Logo comp</h2>
+        </div>
+    )
+}
+
+export default Logo
--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Logo from './logo'
 
 function MainNavigation() {
     return (
