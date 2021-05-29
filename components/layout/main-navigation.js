4191311d0903078605e676ddcb3fd874e51bd50d
Sergio D. Barrera <sdbeng@gmail.com>
2021-05-29 06:50:10 -0700

    fix Link-render Logo comp by wrapping with a tag

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -4,10 +4,13 @@ import Logo from './logo'
 function MainNavigation() {
     return (
         <header>
-        <Logo />
+        <Link href='/'>
+            <a><Logo /></a>
+        </Link>
         <nav>
             <ul>
                 <li><Link href="/posts">Posts</Link></li>
+                <li><Link href="/contact">Contact</Link></li>
             </ul>
         </nav>
             
