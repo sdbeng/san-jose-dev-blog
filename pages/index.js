876510ad9d3095f59c1ec914a8bca9a4497dc480
Sergio D. Barrera <sdbeng@gmail.com>
2021-05-30 23:16:51 -0700

    update featured posts

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import FeaturedPosts from '../components/home-page/featured-posts'
 import Hero from '../components/home-page/hero'
 import styles from '../styles/Home.module.css'
+
+const DUMMY_POSTS = [
+  {slug:'hello-devs', title:'hello devs', image:'coding-event-mesm.jpg', excerpt:'hello devs time to get going', date:'2021-05-29' }
+]
 
 export default function Home() {
   return (
@@ -13,6 +18,7 @@ export default function Home() {
       </Head>
 
         <Hero />
+        <FeaturedPosts posts={DUMMY_POSTS}/>
       
 
       <footer className={styles.footer}>
