b497a14e703dcd4739897e10ac2cdf473a026e4b
Sergio D. Barrera <sdbeng@gmail.com>
2021-06-01 09:30:42 -0700

    return the AllPosts components and pass dummy data temp

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,10 +1,15 @@
 import {useState} from 'react'
+import AllPosts from '../../components/posts/all-posts'
+
+const DUMMY_POSTS = [
+    {slug:'hello-devs', title:'hello devs', image:'coding-event-mesm.jpg', excerpt:'hello devs time to get coding', date:'2021-05-29' },
+    {slug:'wwdc', title:'wwdc', image:'wwdc.jpg', excerpt:'hello devs time to get coding', date:'2021-05-29' },
+    {slug:'nextjs', title:'nextjs is on', image:'nextjs.png', excerpt:'lorem ipsum dojpefdjpo fojepfjef jefjpoefpeoj poufpouefqfoup3ojpoj eofu', date:'2021-05-30' },
+  ]
 
 function AllPostsPage() {
     return (
-        <div>
-            
-        </div>
+        <AllPosts posts={DUMMY_POSTS}/>
     )
 }
 
