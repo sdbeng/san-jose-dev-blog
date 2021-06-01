bf0dd119d1255dd0cf99d318ce2c7cdeb13bad46
Sergio D. Barrera <sdbeng@gmail.com>
2021-06-01 09:26:47 -0700

    add all-posts js to imrove code structure

---
+++ components/posts/all-posts.js
@@ -0,0 +1,11 @@
+import classes from './all-posts.module.css'
+
+function AllPosts() {
+    return (
+        <section>
+            <h1>All Posts</h1>
+        </section>
+    )
+}
+
+export default AllPosts
